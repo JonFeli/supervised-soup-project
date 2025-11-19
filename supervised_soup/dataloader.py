24255c83cbffc010f06e336417c067867135eff6
jo <jonas-felix.reutter@stud.uni-bamberg.de>
2025-11-19 17:02:21 +0100

    SSXP-14 implementation dataloader

---
+++ supervised_soup/dataloader.py
@@ -0,0 +1,144 @@
+#TODO: add/update docstrings
+
+"""
+This module implements the Dataloader by creating train/val 
+DataLoader objects with the necessary preprocessing transforms for 
+ResNet-18, including resizing, cropping, normalization, and batching.
+
+The batch size is currently set to 64
+-----
+
+I have followed roughly these steps: 
+1. Read the picture files.
+2. Decode the JPEG content to RGB grids of pixels.
+3. Convert these into floating-point tensors.
+4. Resize them to a shared size.
+5. Pack them into batches.
+
+Source: https://deeplearningwithpython.io/chapters/chapter08_image-classification/
+
+And the recommendations for resizing and normalizing for pre-trained models from the docs:
+https://docs.pytorch.org/vision/0.12/models.html
+
+"""
+
+import os
+from pathlib import Path
+
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from supervised_soup.config import DATA_PATH
+
+print(DATA_PATH)
+
+# convert path string to Path
+DATA_PATH = Path(os.environ["DATA_PATH"])
+
+
+
+# transform images for baseline
+baseline_transforms = transforms.Compose([
+    transforms.Resize(256),       # resize shorter side to preserve ratio
+    transforms.CenterCrop(224),   # shared size for ResNet
+    transforms.ToTensor(),        # convert to float tensor [0,1]
+    transforms.Normalize(         # standard ImageNet normalization (see docs)
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+# loading the datasets for train and val with ImageFolder
+# ImageFolder automatically reads and decodes JPEGs
+train_dataset = datasets.ImageFolder(
+    root=DATA_PATH / "train",
+    transform=baseline_transforms
+)
+
+val_dataset = datasets.ImageFolder(
+    root=DATA_PATH / "val",
+    transform=baseline_transforms
+)
+
+
+# create batches
+# not sure what batch size would be best for training on colab, we can adjust later
+BATCH_SIZE = 64  
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    # loads data in parallel
+    num_workers=4,
+    # should be true if using GPU, but false if CPU
+    pin_memory=True 
+)
+
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    num_workers=4,
+    pin_memory=True
+)
+
+
+## Test batch loading to test that:
+# Images have the shape ResNet-18 expects → [batch, 3, 224, 224]
+# Labels are integer class IDs → [batch]
+# Images are float32 → required by PyTorch models
+
+batch = next(iter(train_loader))
+images, labels = batch
+
+print("Batch loaded!")
+print("Images shape:", images.shape)
+print("Labels shape:", labels.shape)
+print("Dtype:", images.dtype)
+print("Min/Max:", images.min().item(), images.max().item())
+
+
+# looping through an epoch to test the dataloader for training
+for i, (images, labels) in enumerate(train_loader):
+    if i % 20 == 0:
+        print(f"Batch {i} OK:", images.shape, labels.shape)
+
+
+# print class names and mapping
+print("Classes:", train_dataset.classes)
+print("Class → Index mapping:", train_dataset.class_to_idx)
+
+
+
+# visualizing some images 
+
+def show_image(img_tensor):
+    """
+    Visualizes images and checks if 
+    - images are decoded correctly
+    - colors are correct (RGB, not grayscale or BGR)
+    - transforms didn’t break anything
+    - normalization can be reversed
+    """
+    img = img_tensor.permute(1, 2, 0).numpy()      # CHW → HWC
+    img = (img * 0.229 + 0.485)                    # undo normalization (std, mean)
+    img = np.clip(img, 0, 1)
+
+    plt.imshow(img)
+    plt.axis("off")
+
+images, labels = next(iter(train_loader))
+# to plot 9 images
+plt.figure(figsize=(8, 8))
+for i in range(9):
+    plt.subplot(3, 3, i+1)
+    show_image(images[i].cpu())
+plt.show()
+
+
+
+
